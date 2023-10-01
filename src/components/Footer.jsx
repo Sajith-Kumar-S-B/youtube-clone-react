006d814b5898af64fd2cf573858d20eae4d1476e
Sajith-Kumar-S-B <sajithkumarsb1@gmail.com>
2023-10-01 20:57:10 +0530

    v4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import {MdSubscriptions} from 'react-icons/md'
 
 function Footer() {
   return (
-    <div className='sticky bottom-0 top-0  md:hidden  z-10 flex  items-center justify-center  h-14 px-4 py-6  dark:bg-black'>
-        <div className="w-[100%] flex  justify-evenly  py-[1rem]  h-10 items-center md:hidden">
+    <div className='sticky w-[100%] md:hidden  z-10 flex  items-center justify-center h-16   px-4  bottom-0 pt-2 pb-7  dark:bg-black'>
+        <div className="w-[100%] flex justify-evenly  items-center">
        
       <div> <AiFillHome className="text-white text-2xl"/></div>
       <div> <MdSubscriptions className="text-white text-2xl"/></div>
